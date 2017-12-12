30f4a373b1a1c102971ef87efe606dfccdaeb5b3
tanvibhakta <tanvibhakta@gmail.com>
2017-12-12 21:44:29 +0530

    nuke, restart with new project name

--- newsotherwise/fabfile.py
+++ newsotherwise/fabfile.py
@@ -28,7 +28,7 @@ from fabric.decorators import hosts
 ################
 
 if not hasattr(env, "proj_app"):
-    env.proj_app = real_project_name("newsmanifest")
+    env.proj_app = real_project_name("newsotherwise")
 
 conf = {}
 if sys.argv[0].split(os.sep)[-1] in ("fab", "fab-script.py"):
